27d18827d8ea012f9cf3b9b5802e85ff5b7d67cc
Abhinav Kayastha <abhinavk@metropolia.fi>
2023-08-22 15:39:53 +0300

    fixed loads of issues

--- Ex 1/second_exercise.c
+++ Ex 1/second_exercise.c
@@ -19,27 +19,27 @@ int main()
     printf("Enter your yearly salary limit: ");
     scanf("%f", &yearly_limit_salary);
 
-    printf("Enter your tax percentage in the applied to income over the limit: ");
+    printf("Enter your tax percentage applied to income over the limit: ");
     scanf("%f", &greater_tax_percentage);
 
     for (int i = 0; i < 12; i++)
     {
-        printf("Enter salary for month %d: ", i);
+        printf("\nEnter salary for month %d: ", i + 1);
         scanf("%f", &salary[i]);
         salary_sum += salary[i];
 
         if (salary_sum > yearly_limit_salary)
         {
-            tax_salary[i] = greater_tax_percentage * salary[i];
+            tax_salary[i] = (greater_tax_percentage / 100) * salary[i];
         }
 
         else
         {
-            tax_salary[i] = tax_percentage * salary[i];
+            tax_salary[i] = (tax_percentage / 100) * salary[i];
         }
 
-        printf("Salary of month %d: ", salary[i]);
-        printf("Taxed income of month %d: ", tax_salary[i]);
+        printf("\nSalary of month %d: %.2f: ", i + 1, salary[i]);
+        printf("\nTaxed income of month %d: %.2f\n", i + 1, tax_salary[i]);
     }
 
     return 0;
