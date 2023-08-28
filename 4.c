b273ea9b6c4efd3217eb7605703ed7230bef2b59
Abhinav Kayastha <abhinavk@metropolia.fi>
2023-08-28 15:12:35 +0300

    finished with 4th ex and refactoring exercises

--- 4.c
+++ 4.c
@@ -2,3 +2,42 @@
 // Created by Abhinav Kayastha on 28/08/2023.
 //
 
+#include <stdio.h>
+
+int read_integer(void)
+{
+    float num;
+    printf("\nEnter a positive integer or negative number to quit: ");
+    scanf("%f", &num);
+
+    return num;
+}
+
+int main()
+{
+    float count = 0;
+    float sum = 0;
+    float avg;
+
+    while (1)
+    {
+        float num = read_integer();
+
+        if (num < 0)
+        {
+            break;
+        }
+
+        else
+        {
+            count += 1;
+            sum += num;
+        }
+    }
+
+    avg = sum/count;
+
+    printf("\nYou entered %.f positive numbers. The average is: %.2f", count, avg);
+
+    return 0;
+}
